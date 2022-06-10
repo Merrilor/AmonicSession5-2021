e3fa2813fd2b964aca3de6eaaee1599559beeea3
Timeset <drakozavr.hy@mail.ru>
2022-06-10 20:59:01 +0300

    Add project files.

---
+++ Session5/Windows/AmenitiesReport.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Session5
+{
+    /// <summary>
+    /// Interaction logic for AmenitiesReport.xaml
+    /// </summary>
+    public partial class AmenitiesReport : Window
+    {
+
+        public List<Tickets> TicketList;
+
+        public List<AmenityReport> AmenitiesReportList { get; set; } = new List<AmenityReport>();
+
+        public AmenitiesReport()
+        {
+            InitializeComponent();
+
+            FlightIdTextBox.Text = "";
+
+            FromDateTextBox.Text = "01.10.2018";
+            ToDateTextBox.Text = "10.10.2018";
+
+        }
+
+        private void MakeReportButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(FromDateTextBox.Text == string.Empty || ToDateTextBox.Text == string.Empty)
+            {
+                MessageBox.Show("Необходимо ввести обе даты", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            DateTime FromDate;
+            DateTime ToDate;
+
+            try
+            {
+                FromDate = DateTime.Parse(FromDateTextBox.Text);
+                ToDate = DateTime.Parse(ToDateTextBox.Text);
+            }
+            catch 
+            {
+                MessageBox.Show("Некорректный формат даты", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+               
+            }
+
+
+            Func<Tickets, bool> FlightNumberFilter = t=> true;
+            if(FlightIdTextBox.Text != string.Empty)
+            {
+                FlightNumberFilter = t => t.Schedules.FlightNumber == FlightIdTextBox.Text;
+            }
+
+            var entities = new Session5Entities();
+            TicketList = entities.Tickets
+                .Where(t => t.Schedules.Date >= FromDate && t.Schedules.Date <= ToDate && t.Confirmed == true)
+                .AsEnumerable()
+                .Where(t=> FlightNumberFilter(t))
+                .ToList();
+
+            
+
+            AmenitiesReportList.Clear();
+            foreach (var cabinType in entities.CabinTypes)
+            {
+
+
+                var report = new AmenityReport
+                {
+
+                    CabinType = cabinType.Name,
+                    ExtraBlanketCount = TicketList.Where(t=> t.CabinTypes.Name == cabinType.Name && t.AmenitiesTickets.Select(at=>at.Amenities.Service).Contains("Extra Blanket")).Count(),
+                    NextSeatFreeCount = TicketList.Where(t=> t.CabinTypes.Name == cabinType.Name && t.AmenitiesTickets.Select(at=>at.Amenities.Service).Contains("Next Seat Free")).Count(),
+                    TwoSeatsFreeCount = TicketList.Where(t=>t.CabinTypes.Name == cabinType.Name && t.AmenitiesTickets.Select(at=>at.Amenities.Service).Contains("Two Neighboring Seats Free")).Count(),
+                    TabletRentalCount = TicketList.Where(t=> t.CabinTypes.Name == cabinType.Name && t.AmenitiesTickets.Select(at=>at.Amenities.Service).Contains("Tablet Rental")).Count(),
+                    LaptopRentalCount = TicketList.Where(t=> t.CabinTypes.Name == cabinType.Name && t.AmenitiesTickets.Select(at=>at.Amenities.Service).Contains("Laptop Rental")).Count(),
+                    LoungeAccessCount = TicketList.Where(t=> t.CabinTypes.Name == cabinType.Name && t.AmenitiesTickets.Select(at=>at.Amenities.Service).Contains("Lounge Access")).Count(),
+                    SoftDrinksCount = TicketList.Where(t=> t.CabinTypes.Name == cabinType.Name && t.AmenitiesTickets.Select(at=>at.Amenities.Service).Contains("Soft Drinks")).Count(),
+                    PremiumHeadphonesRentalCount = TicketList.Where(t => t.CabinTypes.Name == cabinType.Name && t.AmenitiesTickets.Select(at => at.Amenities.Service).Contains("Premium Headphones Rental")).Count(),
+                    ExtraBagCount = TicketList.Where(t => t.CabinTypes.Name == cabinType.Name && t.AmenitiesTickets.Select(at => at.Amenities.Service).Contains("Extra Bag")).Count(),
+                    FastCheckingLaneCount = TicketList.Where(t => t.CabinTypes.Name == cabinType.Name && t.AmenitiesTickets.Select(at => at.Amenities.Service).Contains("Fast Checkin Lane")).Count(),
+                    Wifi50mbCount = TicketList.Where(t => t.CabinTypes.Name == cabinType.Name && t.AmenitiesTickets.Select(at => at.Amenities.Service).Contains("Wi-Fi 50 mb")).Count(),
+                    Wifi250mbCount = TicketList.Where(t => t.CabinTypes.Name == cabinType.Name && t.AmenitiesTickets.Select(at => at.Amenities.Service).Contains("Wi-Fi 250 mb")).Count(),
+
+
+
+                };
+
+
+                AmenitiesReportList.Add(report);
+
+            }
+
+            AmenitiesDataGrid.ItemsSource = new List<AmenityReport>();
+            AmenitiesDataGrid.ItemsSource = AmenitiesReportList;
+
+        }
+
+
+
+        private void FlightIdTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            char character = (char)KeyInterop.VirtualKeyFromKey(e.Key);
+
+            if (!char.IsDigit(character) && e.Key != Key.Back)
+                e.Handled = true;
+           
+
+        }
+    }
+
+
+    public class AmenityReport
+    {
+        
+        public string CabinType { get; set; }
+
+        public int ExtraBlanketCount { get; set; }
+        public int NextSeatFreeCount { get; set; }
+        public int TwoSeatsFreeCount { get; set; }
+        public int TabletRentalCount { get; set; }
+        public int LaptopRentalCount { get; set; }
+        public int LoungeAccessCount { get; set; }
+        public int SoftDrinksCount { get; set; }
+        public int PremiumHeadphonesRentalCount { get; set; }
+        public int ExtraBagCount { get; set; }
+        public int FastCheckingLaneCount { get; set; }
+        public int Wifi50mbCount { get; set; }
+        public int Wifi250mbCount { get; set; }
+
+
+
+    }
+
+
+}
---
+++ Session5/Windows/PurchaseAmenitiesWindow.xaml.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Session5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class PurchaseAmenitiesWindow : Window
+    {
+
+        private readonly DateTime CurrentDateTime = new DateTime(2017, 10, 14,0,0,1);
+
+
+        public double ItemSelectedPrice { get; set; }
+
+        public double TaxesPrice { get; set; }
+
+        public double TotalPayable { get; set; } 
+
+        private Tickets SelectedTicket;
+
+
+        public PurchaseAmenitiesWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void BookingReferenceSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            var entities = new Session5Entities();
+
+            var FlightList = entities.Tickets
+                .Where(t => t.BookingReference == BookingReferenceTextBox.Text && t.Confirmed == true)
+                .AsEnumerable()
+                .Where(t=>(t.Schedules.Date.Add(t.Schedules.Time)) - TimeSpan.FromHours(24) > CurrentDateTime)
+                .ToList();
+
+            if(FlightList.Count == 0)
+            {
+                MessageBox.Show("Не найдено ни одного полета");
+                return;
+            }
+
+            FlightComboBox.ItemsSource = FlightList;
+            FlightComboBox.SelectedIndex = 0;
+            
+            
+        }
+
+        private  void ShowAmenitiesButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (FlightComboBox.SelectedItem is null)
+                return;
+
+
+            AmenitiesWrapPanel.Children.Clear();
+
+            var entities = new Session5Entities();           
+
+            var amenities = entities.Amenities.ToList();
+
+
+            var AmenityCheckBoxes = new List<CheckBox>();
+
+            foreach (var item in amenities)
+            {
+
+                CheckBox newAmenityCheckBox = new CheckBox();
+                newAmenityCheckBox.Content = item.Service.ToString();
+                newAmenityCheckBox.Margin = new Thickness(10);
+
+                if (((Tickets)FlightComboBox.SelectedItem).CabinTypes.Amenities.Select(a=>a.Service).Contains(item.Service) || item.Price == 0)
+                {
+                    newAmenityCheckBox.Content += "(Free)";
+                    newAmenityCheckBox.Opacity = 0.5;
+                    newAmenityCheckBox.IsChecked = true;
+                    newAmenityCheckBox.IsEnabled = false;
+                }
+                else
+                {
+                    newAmenityCheckBox.Content += $"(${item.Price:0.})";
+
+                    if(((Tickets)FlightComboBox.SelectedItem).AmenitiesTickets.Select(at=> at.AmenityID).Contains(item.ID))
+                    {
+                        newAmenityCheckBox.IsChecked = true;
+                    }
+
+                    newAmenityCheckBox.Checked += CalculatePay;
+                    newAmenityCheckBox.Unchecked += CalculatePay;
+
+                }
+
+                newAmenityCheckBox.Tag = item.Price.ToString();
+                AmenityCheckBoxes.Add(newAmenityCheckBox);
+
+            }
+
+            
+
+            AmenityCheckBoxes = AmenityCheckBoxes.OrderBy(cb => cb.Opacity).ToList();
+            foreach (var checkBox in AmenityCheckBoxes)
+            {
+                AmenitiesWrapPanel.Children.Add(checkBox);
+            }
+
+
+            SelectedTicket = (Tickets)FlightComboBox.SelectedItem;
+
+
+        }
+
+        private void CalculatePay(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+
+            if (checkBox.IsChecked == true)
+            {
+                ItemSelectedPrice += (double)decimal.Parse(checkBox.Tag.ToString());
+            }
+            else
+            {
+                ItemSelectedPrice -= (double)decimal.Parse(checkBox.Tag.ToString());
+            }
+            
+
+            TaxesPrice = (double)ItemSelectedPrice / 100 * 5;
+
+            TotalPayable = ItemSelectedPrice + TaxesPrice;
+
+
+            SelectedItemsPriceTextBlock.Text = $"Items selected: ${ItemSelectedPrice}";
+
+            TaxesPriceTextBlock.Text = $"Duties and taxes: ${TaxesPrice}";
+
+            TotalPayableTextBlock.Text = $"Total payable: ${TotalPayable}";
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if(SelectedTicket is null)
+            {
+                return;
+            }
+
+            var entities = new Session5Entities();
+
+            var deleteAmenitiesTickets = entities.AmenitiesTickets.Where(at => at.TicketID == SelectedTicket.ID);
+            entities.AmenitiesTickets.RemoveRange(deleteAmenitiesTickets);
+            entities.SaveChanges();
+
+
+            foreach (var amenityCheckBox in AmenitiesWrapPanel.Children)
+            {
+                CheckBox checkBox = amenityCheckBox as CheckBox;
+                string AmenityName = checkBox.Content.ToString().Remove(checkBox.Content.ToString().IndexOf('('));
+                string AmenityPrice = checkBox.Tag.ToString().Replace(',', '.');
+
+                if (checkBox.Opacity != 1 || checkBox.IsChecked == false )
+                {
+                    continue;
+                }
+
+
+                AmenitiesTickets newAmenitiesTickets = new AmenitiesTickets
+                {
+
+                    AmenityID = entities.Amenities.Where(a => a.Service == AmenityName).Single().ID,
+                    TicketID = SelectedTicket.ID,
+                    Price = decimal.Parse(checkBox.Tag.ToString())
+
+                };
+
+                entities.AmenitiesTickets.Add(newAmenitiesTickets);
+
+                try
+                {
+                    entities.SaveChanges();
+                    
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка");
+                    return;
+                }
+
+                
+
+            }
+
+            MessageBox.Show("Данные успешно сохранены");
+
+            
+
+        }
+
+        private void AmenitiesReportButton_Click(object sender, RoutedEventArgs e)
+        {
+            new AmenitiesReport().ShowDialog();
+           
+        }
+    }
+}
